4797ce2e6667297e27424eee06588bee7d4a8af2
Erhahon Imafidon <mafiosco4@gmail.com>
2026-06-27 21:22:59 +0100

    Add debounced search functionality to Search component

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,22 +2,23 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
     const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
 
-    const handleSearch = (term: string) => {
+    const handleSearch = useDebouncedCallback((term: string) => {
         const params = new URLSearchParams(searchParams);
-        const pathname = usePathname();
-        const { replace } = useRouter();
-
+        console.log(`searching: ${term}`);
         if (term) {
             params.set('query', term);
         } else {
             params.delete('query');
         }
         replace(`${pathname}?${params.toString()}`);
-    };
+    }, 300);
 
     return (
         <div className="relative flex flex-1 flex-shrink-0">
@@ -34,3 +35,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
         </div>
     );
 }
+
+export const debounce = <Arg extends unknown[]>(
+    func: (...args: Arg) => void,
+    delay: number
+) => {
+    let timeoutId: ReturnType<typeof setTimeout>;
+    return (...args: Arg) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => func(...args), delay);
+    };
+};
